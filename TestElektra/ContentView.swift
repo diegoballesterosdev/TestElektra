8fe5a38c86d17a30bf0f66a45fcd6c41dfe3d4e2
diegoballesterosdev <diegoballesteros.dev@gmail.com>
2024-09-08 10:58:45 -0600

    Initial Commit

---
+++ TestElektra/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestElektra
+//
+//  Created by Diego Ballesteros on 08/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestElektra/TestElektraApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestElektraApp.swift
+//  TestElektra
+//
+//  Created by Diego Ballesteros on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TestElektraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
